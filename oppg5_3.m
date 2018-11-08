6470f49c7a67e1a8393716b863b4a9d084411b2b
hakfj <haakonflisnes@gmail.com>
2018-11-08 18:25:50 +0100

    Fikset formatteringen på bode plottet

--- oppg5_3.m
+++ oppg5_3.m
@@ -1,4 +1,6 @@
-%% 5.3 a
+% 5.3 a
+
+close all
 
 s = tf('s')
 % syms s
@@ -19,11 +21,36 @@ Hpd = Kpd*(Td*s+1)/(Tf*s+1);
 
 Hol = Hb*Hpd
 
-% bode(Hol) ja
+% bode(Hol)
+% set(gca,'FontSize',12,'linewidth',1.0)
+
+
 [Gm,Pm,Wcg,Wcp] = margin(Hol)
 margin(Hol)
+% set(gca,'FontSize',12,'linewidth',1.0)
+%title("Bode Diagram, Pm = 50 deg (at 0.1 rad/s)",'Interpreter','latex')
+bode(Hol)
 
+Hsys = (K*Kpd)/(s*(Tf*s+1))
 
+[mag,phase,wout] = bode(Hsys);
+figure
+subplot(2,1,1)
+semilogx(wout, 20*log10(squeeze(mag)), '-', 'LineWidth',1.5)
+axis([10^-3, 10, -60, 40])
+set(gca,'FontSize',12,'linewidth',1.0)
+title("Bode Diagram",'Interpreter','latex')
+ylabel("Magnitude (DB)",'Interpreter','latex','FontSize', 15)
+grid
+subplot(2,1,2)
+semilogx(wout, squeeze(phase), '-', 'LineWidth',1.5)
+axis([10^-3, 10, -180, -90])
+% Trenger kode for å stille på ticks her
+yticks([-180 -130 -90])
+set(gca,'FontSize',12,'linewidth',1.0)
+ylabel("Phase (deg)",'Interpreter','latex','FontSize', 15)
+xlabel("Frequency (rad/s)",'Interpreter','latex','FontSize', 15)
+grid
 
 
 
