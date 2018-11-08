69576d82ea47aca0e5c2f6f2a9b6e263e06c41c1
Tjore <marius_tjore@hotmail.com>
2018-11-08 18:23:49 +0100

    Endret plot i 5.3
    Fjernet de doble plottene og la de sammen til ett plot.

--- oppg5_3d.m
+++ oppg5_3d.m
@@ -12,31 +12,20 @@ sim("ship_5_3_d.slx")
 %% plotting
 
 fig1 = figure
-plot(compass,'b','LineWidth',1.5)
+plot(compass,'LineWidth',1.5)
+hold on
+plot(u,'LineWidth',1.5)
 grid
-title("Compass heading",'Interpreter','latex')
-legend({'Heading $\psi$'},'Interpreter','latex','location','northeastoutside');
+title("$\psi_r = 30^\circ$ with wave disturbance",'Interpreter','latex')
+legend({'Heading $\psi$', 'Controller output'},'Interpreter','latex','location','northeastoutside');
 xlabel("Time (Seconds)",'Interpreter','latex','FontSize', 15)
-ylabel("$\psi$ (Degrees)",'Interpreter','latex','FontSize', 15)
+ylabel("(Degrees)",'Interpreter','latex','FontSize', 15)
 set(gcf, 'Position', [100, 100, 700, 400])
 set(gca,'FontSize',12,'linewidth',1.0)
 
-
-fig2 = figure
-plot(u,'b','LineWidth',1.5)
-grid
-title("Controller output",'Interpreter','latex')
-legend({'Rudder input'},'Interpreter','latex','location','northeastoutside');
-xlabel("Time (Seconds)",'Interpreter','latex','FontSize', 15)
-ylabel("$\delta$ (Degrees)",'Interpreter','latex','FontSize', 15)
-set(gcf, 'Position', [100, 100, 700, 400])
-set(gca,'FontSize',12,'linewidth',1.0)
 
 %% saves figures if there is none
 
-if exist('5_3d_compass.eps', 'file') ~= 2
-    saveas(fig1,'5_3d_compass.eps','eps');
-end
-if exist('5_3d_controllerOutput.eps', 'file') ~= 2
-    saveas(fig1,'5_3d_controllerOutput.eps','eps');
+if exist('5_3d_OutputAndHeading.eps', 'file') ~= 2
+    saveas(fig1,'5_3d_OutputAndHeading.eps','epsc');
 end
